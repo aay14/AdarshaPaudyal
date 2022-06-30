37aa84bf26e54eb97fb1a19a426a535a00e55fed
Aap_59 <92156762+Aap59@users.noreply.github.com>
2022-06-30 21:26:16 +0545

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,26 @@
-/*===== MENU SHOW =====*/
-function websiteVisits(response) {
-  document.querySelector("#visits").textContent = response.value;
+var counterContainer = document.querySelector(".website-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
 }
+counterContainer.innerHTML = visitCount;
+
+// Adding onClick event listener
+resetButton.addEventListener("click", () => {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+  counterContainer.innerHTML = visitCount;
+});
+
+/*===== MENU SHOW =====*/
+
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId);
